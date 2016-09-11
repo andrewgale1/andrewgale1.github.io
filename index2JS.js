d934b40b025e35cac6d9e6bde8a03bcc07abe21e
Andrew Gale <andrewgale1@users.noreply.github.com>
2016-09-11 01:29:59 -0700

    Add files via upload

---
+++ index2JS.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+ $(".glyphicon").fadeOut(3000,function(){
+
+
+ $(".glyphicon-eye-open").fadeIn(2000);
+ $(".glyphicon-fire").fadeIn(2000);
+ $(".glyphicon-folder-open").fadeIn(2000);
+ $(".glyphicon-check").fadeIn(2000);
+
+ });
+
+ 
+ 
+});
+
+
+
+
+
+
+
+
+
+
+
