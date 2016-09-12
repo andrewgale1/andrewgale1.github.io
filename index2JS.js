6c6b10c17ef78c23ed5247a77f33da111280c524
Andrew Gale <andrewgale1@users.noreply.github.com>
2016-09-11 23:13:06 -0700

    Update index2JS.js

--- index2JS.js
+++ index2JS.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
 
- $(".glyphicon").fadeOut(3000,function(){
+$(".glyphicon").fadeOut(2000,function(){
 
+    $(".glyphicon-eye-open").delay(1000).fadeIn(3000);
+    $(".glyphicon-fire").delay(4000).fadeIn(3000);
+    $(".glyphicon-folder-open").delay(6000).fadeIn(3000);
+    $(".glyphicon-check").delay(8000).fadeIn(3000, function(){
 
- $(".glyphicon-eye-open").fadeIn(2000);
- $(".glyphicon-fire").fadeIn(2000);
- $(".glyphicon-folder-open").fadeIn(2000);
- $(".glyphicon-check").fadeIn(2000);
+        $(".glyphicon-eye-open").css("color","cadetblue").fadeIn(3000);
+        $(".glyphicon-fire").css("color","maroon").fadeIn(3000);
+        $(".glyphicon-folder-open").css("color","indigo").fadeIn(3000);
+        $(".glyphicon-check").css("color","darkgreen").fadeIn(3000);
+        });
 
- });
-
- 
- 
+    });
+      
 });
 
 
